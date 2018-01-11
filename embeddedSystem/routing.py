3d0abb62eec1cfb40b3306111af5358f6c15b93b
ssdoz2sk <mybalance22@gmail.com>
2018-01-11 12:22:17 +0800

    修改websocket 上傳網址

--- embeddedSystem/routing.py
+++ embeddedSystem/routing.py
@@ -3,8 +3,8 @@ from channels import route_class
 from project.consumers import DataConsumer, SensorConsumer, DeviceConsumer, ProjectConsumer
 
 channel_routing = [
-    route_class(DataConsumer, path=r'^/data/(?P<token>[^/.]+)/$'),
-    route_class(SensorConsumer, path=r'^/sensor/(?P<pk>[^/.]+)/$'),
-    route_class(DeviceConsumer, path=r'^/device/(?P<pk>[^/.]+)/$'),
-    route_class(ProjectConsumer, path=r'^/project/(?P<pk>[^/.]+)/$'),
+    route_class(DataConsumer, path=r'^/ws/data/(?P<token>[^/.]+)/$'),
+    route_class(SensorConsumer, path=r'^/ws/sensor/(?P<pk>[^/.]+)/$'),
+    route_class(DeviceConsumer, path=r'^/ws/device/(?P<pk>[^/.]+)/$'),
+    route_class(ProjectConsumer, path=r'^/ws/project/(?P<pk>[^/.]+)/$'),
 ]
